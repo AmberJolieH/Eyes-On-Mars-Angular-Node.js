eb6a6b1dc3f0093a2992670a41febc59120c071c
amberjolie <amberjolieh@gmail.com>
2021-05-24 19:38:20 -0500

    footer works

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  template: `
+    <p>
+      footer works!
+    </p>
+  `,
+  styles: []
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  ngOnInit() {}
 }
