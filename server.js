ac248afb162509f94bbf68075a5880d91cbfc431
amberjolie <amberjolieh@gmail.com>
2021-05-25 21:31:00 -0500

    gitignore

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const app = express();
 // get post routes for Nasa API
 const api = require('./server/routes/api');  
 
+
 //use middleware
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, 'dist'))); 
 app.use('/api', api);
 
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+
+
 //for http requests
 const axios = require('axios');
-const PostAPI = 'https://jsonplaceholder.typicode.com'
+const PostAPI = 'https://api.nasa.gov/mars-photos/api/v1/rovers'
 
 
 // GET POST
-router.get('/', (req, res)=>{
+router.post('/', (req, res)=>{
     console.log('hi');
-    axios.get(`${PostAPI}/posts`).then(posts=>{
+    const {rover, date} = req.body
+    const dateString = new Date(date)
+    const month = dateString.getMonth() + 1
+    const day = dateString.getDate()
+    const year = dateString.getFullYear()
+    axios.get(`${PostAPI}/${rover}/photos?earth_date=${year}-${month}-${day}&api_key=${APIKEY}`).then(posts=>{
         console.log(posts.data);
 
         res.json(posts.data);
